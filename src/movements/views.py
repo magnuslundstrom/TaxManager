19520f8e498a5eae1b583704ac7f2380b8fabc15
magnuslundstrom <magnuslundstromdk@gmail.com>
2021-02-24 23:28:35 +0100

    Update item form added

--- src/movements/views.py
+++ src/movements/views.py
@@ -27,13 +27,14 @@ def create(request):
         try:
             newMovement = Movement()
             newMovement.amount = request.POST['amount']
-            newMovement.sender = request.POST['sender']
-            newMovement.receiver = request.POST['receiver']
+            newMovement.partner = request.POST['partner']
+            newMovement.invoiceLink = request.POST['invoiceLink']
             newMovement.user = request.user
+            newMovement.movementDate = request.POST['movementDate']
             newMovement.save()
             context['message'] = 'Successfully created'
         except:
-            context['errors'] = 'You provided wrong datatypes. Please provide num - string - string'
+            context['errors'] = 'You provided wrong datatypes.'
 
     # provides a list of companies to choose from
     if request.method == 'GET':
@@ -47,7 +48,7 @@ def delete(request):
     if request.method == "POST":
         id = request.POST['id'] or 0
         movement = Movement.objects.get(id=id)
-        if movement.user.id == request.user.id:
+        if movement.user == request.user:
             movement.delete()
 
     return HttpResponseRedirect(reverse('movements:movements'))
@@ -55,9 +56,24 @@ def delete(request):
 
 @login_required
 def displayItem(request, id):
+    if request.method == "POST":
+        movementId = request.POST["movementId"]
+        movement = Movement.objects.get(id=movementId)
+        if movement and movement.user == request.user:
+            movement.amount = request.POST['amount']
+            movement.partner = request.POST['partner']
+            movement.invoiceLink = request.POST['invoiceLink']
+            movement.movementDate = request.POST['movementDate']
+            movement.save()
+        else:
+            return render(request, 'item.html')
+
+    companies = Company.objects.all().filter(
+        relatedToUser=request.user)
     movement = Movement.objects.get(id=id)
     context = {
-        'movement': movement
+        'movement': movement,
+        'companies': companies
     }
     if movement.user.id == request.user.id:
         return render(request, 'item.html', context)
